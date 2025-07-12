4592e63cbe656a8cf24f7575232d99b7f1d96c97
tchivaura <tnasshechivaurah@gmail.com>
2025-07-12 15:46:57 +0200

    comentscommit

--- src/App.js
+++ src/App.js
@@ -1,32 +1,33 @@
 import SearchableList from "./components/SearchableList";
-
-const data = [
-  { name: 'Alice', email: 'alice@example.com' },
-  { name: 'Bob', email: 'bob@example.com' },
-];
-
-const headers = ['Name', 'Email'];
-
-function renderRow(item, index) {
-  return (
-    <tr key={index}>
-      <td>{item.name}</td>
-      <td>{item.email}</td>
-    </tr>
-  );
-}
+//data for searchable list
+// const data = [
+//   { name: 'Alice', email: 'alice@example.com' },
+//   { name: 'Bob', email: 'bob@example.com' },
+// ];
+//headers for searchable list
+// const headers = ['Name', 'Email'];
+//render prop for searchable list
+// function renderRow(item, index) {
+//   return (
+//     <tr key={index}>
+//       <td>{item.name}</td>
+//       <td>{item.email}</td>
+//     </tr>
+//   );
+// }
 
 function App() {
   return (
-    <div className="container mt-4">
-      <SearchableList
-        data={data}
-        headers={headers}
-        searchableFields={['name', 'email']}
-        renderRow={renderRow}
-      />
-    </div>
-  );
-}
+    <></>
+    // <div className="container mt-4">
+    //   <SearchableList
+    //     data={data}
+    //     headers={headers}
+    //     searchableFields={['name', 'email']}
+    //     renderRow={renderRow}
+    //   />
+    // </div>
+   );
+ }
 
 export default App;
